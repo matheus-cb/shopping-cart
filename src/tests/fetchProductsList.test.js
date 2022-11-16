5cf38ff534b53a0877983f2ab4869faba8f097d5
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2022-11-16 19:37:11 -0300

    requesito 1 teste fetchProductsList

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,28 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function')
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    // .toBeCalled para garantir que a mock seja chamada.
+    await fetchProductsList('computador');
+    expect(fetch).toBeCalled()
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    // .toBeCalledWith() para mock com argumentos específicos
+    const API = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProductsList('computador');
+    expect(fetch).toBeCalledWith(API)
   });
 
-  // it('...', () => {
-  // });
+  it('Teste se o retorno da função fetchProductsList com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    // .toReturnWith(value) para garantir que a mock retorne um valor específico
+    const expected = await fetchProductsList('computador');
+    expect(expected).toReturnWith(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: "Termo de busca não informado".', async () => {
+    expect(error.message).toBe('Termo de busca não informado');
+  });
 });
