7969c60a094ad62206e4940bafe6abeeab35832b
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2022-11-18 19:24:23 -0300

    requisito 04 ok

--- src/main.js
+++ src/main.js
@@ -1,40 +1,37 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import { createCustomElement, createProductElement } from './helpers/shopFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
-console.log(await fetchProductsList('computador'));
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-// const loadProducts = () => {
-//   const sectionMain = document.querySelector('.products');
-//   const div = document.createElement('div');
-//   div.className = 'loading';
-//   div.innerText = 'carregando...';
-//   // div.display = none;
-//   // return console.log(div);
-//   sectionMain.appendChild(div);
-// };
+const loadProducts = () => {
+  const sectionMain = document.querySelector('.products');
+  const div = document.createElement('div');
+  div.className = 'loading';
+  div.innerText = 'carregando...';
+  sectionMain.appendChild(div);
+};
 
-const carregado = () => {
-  const divC = document.querySelector('#loading');
-  divC.remove();
+const loaded = () => {
+  const divC = document.querySelector('.loading');
   console.log(divC);
+  divC.remove();
 };
 
 const displayProducts = async () => {
+  loadProducts();
   // cria uma variável que contem uma lista de objetos
   const listObj = await fetchProductsList('computador');
-  carregado();
   // seleciona o local que será o pai da lista dos produtos
   const sectionProducts = document.querySelector('.products');
   // para cada elemento da lista de obj cria uma section com descrições, img e preço do produto ...
   listObj.forEach((element) => {
     sectionProducts.appendChild(createProductElement(element));
   });
+  loaded();
 };
 
-displayProducts();
-// window.onload = () => {
-//   // loadProducts();
-// };
+window.onload = async () => {
+  await displayProducts();
+};
