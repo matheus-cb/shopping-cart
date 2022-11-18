0be3cd68460edc4b059235d32a2b61b189c23499
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2022-11-18 20:57:42 -0300

    requisito 05 ok

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 // cria mensagem de carregando
 const loadProducts = () => {
   const sectionMain = document.querySelector('.products');
-  const div = document.createElement('div');
-  div.className = 'loading';
-  div.innerText = 'carregando...';
-  sectionMain.appendChild(div);
+  const messageLoad = document.createElement('h1');
+  messageLoad.className = 'loading';
+  messageLoad.innerText = 'carregando...';
+  sectionMain.appendChild(messageLoad);
 };
 
 // remove mensagem de carregando
@@ -23,14 +23,22 @@ const loaded = () => {
 // chama as funções loadProfucts e loaded, e também mostra os produtos na tela
 const displayProducts = async () => {
   loadProducts();
-  // cria uma variável que contem uma lista de objetos
-  const listObj = await fetchProductsList('computador');
-  // seleciona o local que será o pai da lista dos produtos
-  const sectionProducts = document.querySelector('.products');
-  // para cada elemento da lista de obj cria uma section com descrições, img e preço do produto ...
-  listObj.forEach((element) => {
-    sectionProducts.appendChild(createProductElement(element));
-  });
+  try {
+    // cria uma variável que contem uma lista de objetos
+    const listObj = await fetchProductsList('computador');
+    // seleciona o local que será o pai da lista dos produtos
+    const sectionProducts = document.querySelector('.products');
+    // para cada elemento da lista de obj cria uma section com descrições, img e preço do produto ...
+    listObj.forEach((element) => {
+      sectionProducts.appendChild(createProductElement(element));
+    });
+  } catch (error) {
+    const sectionMain = document.querySelector('.products');
+    const menssageError = document.createElement('h1');
+    menssageError.className = 'error';
+    menssageError.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    sectionMain.appendChild(menssageError);
+  }
   loaded();
 };
 
