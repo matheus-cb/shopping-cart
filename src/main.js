89e9c8bc31a75b6e9306aa84d6e2d01dd13bb575
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2022-11-18 16:54:15 -0300

    requisito 03 ok

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
-// import { createProductElement } from './helpers/shopFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
-console.log(fetchProductsList('computador'));
+console.log(await fetchProductsList('computador'));
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const displayProducts = async () => {
+  // cria uma variável que contem uma lista de objetos
+  const listObj = await fetchProductsList('computador');
+  // seleciona o local que será o pai da lista dos produtos
+  const sectionProducts = document.querySelector('.products');
+  // para cada elemento da lista de obj cria uma section com descrições, img e preço do produto
+  listObj.forEach((element) => {
+    sectionProducts.appendChild(createProductElement(element));
+  });
+};
+
+window.onload = function () {
+  displayProducts();
+};
