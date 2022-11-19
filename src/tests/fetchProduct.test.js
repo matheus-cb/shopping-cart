120fe50ef10b66935873cd088071dfb8ea7a8736
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2022-11-19 14:10:40 -0300

    requisito 07 +-

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,23 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('Teste se "fetchProduct" é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+  it('Execute a função "fetchProduct" com o argumento do produto "MLB1405519561" e teste se fetch foi chamada', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toBeCalled();
+  });
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    const API = 'https://api.mercadolibre.com/items/MLB1405519561';
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toBeCalledWith(API);
+  });
+  it('Teste se o retorno da função fetchProduct com o argumento do produto "MLB1405519561" é uma estrutura de dados igual ao objeto produto que já está importado no arquivo.', async () => {
+    const expected = await fetchProduct('MLB1405519561');
+    expect(expected).toEqual(product);
+  });
+  it('Teste se, ao chamar a função "fetchProduct" sem argumento, retorna um erro com a mensagem: "ID não informado"', async () => {
+    await expect(fetchProduct()).rejects.toThrow();
   });
 });
