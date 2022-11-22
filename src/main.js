f96a4d98924ff43fa70b8de799110dc46cc85b57
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2022-11-22 09:39:48 -0300

    requisito 09 ok

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { getSavedCartIDs, saveCartID } from './helpers/cartFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
@@ -43,31 +43,47 @@ const displayProducts = async () => {
   loaded();
 };
 
-// adiciona os produtos ao carrinho de compras
-const addProductsToLS = async () => {
-  // capitura o botão adicionar ao carrinho
+// seleciona local para os produtos
+const selectSpaceCart = (product) => {
+  const li = createCartProductElement(product);
+  const cart = document.querySelector('.cart__products');
+  return cart.appendChild(li);
+};
+
+// adiciona os produtos ao carrinho de compras e ao localStorage
+const addProducts = async () => {
+  // capitura os botões deixando eles em um array
   const btnProducts = document.querySelectorAll('.product__add');
   btnProducts.forEach((element, i) => {
     element.addEventListener('click', async () => {
+      // capitura os prdutos deixando eles em um array
       const productId = document.querySelectorAll('.product__id');
+      // saveCartId para salvar o id do produto ao localStorage
       saveCartID(productId[i].innerText);
+      // capitura o id e usa a selectSpaceCart() p/ mostrar aonde vai ficar o local dos produtos
       const id = element.parentElement.firstElementChild.innerHTML;
       const product = await fetchProduct(id);
-      const li = createCartProductElement(product);
-      const carrinho = document.querySelector('.cart__products');
-      carrinho.appendChild(li);
+      selectSpaceCart(product);
     });
   });
 };
 
 // adiciona o produto do local storage para o carrinho
-const addProductsToCart = () => {
+const addProductsToLS = () => {
+  // capitura os id's dos produtos no localStrorage
   const productsLS = localStorage.cartProducts;
-  console.log(productsLS);
+  // cria um array com os produtros do localStorage
+  const arrayProducts = getSavedCartIDs(productsLS);
+  arrayProducts.map(async (element) => {
+    // capitura as infos dos produtros
+    const productToArray = await fetchProduct(element);
+    // selectSpaceCart() p/ mostrar aonde vai ficar o local dos produtos
+    selectSpaceCart(productToArray);
+  });
 };
 
 window.onload = async () => {
   await displayProducts();
-  await addProductsToLS();
-  addProductsToCart();
+  await addProducts();
+  addProductsToLS();
 };
